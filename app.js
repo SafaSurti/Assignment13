e21294d42eb1221566dcc7d7847dbe3e19bc07d9
SafaSurti <safamushtaqsurti@gmail.com>
2024-09-09 17:36:53 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,156 @@
+// chapter 1
+// Question 1
+
+// alert("Hello")
+
+// Question 2
+//  alert(" JavaScript Alert  \n \n Error! Please enter a valid password")
+
+//  Question 3
+
+
+// alert(" JavaScript Alert \n \n Welcome to JS land  \n Happy coding")
+// Question 4 part a
+
+// alert(" JavaScript Alert \n \n Welcome to JS land")  
+// Question 4 part b
+
+// alert(" JavaScript Alert \n \n Happy Coding! \n Prevent this page from creating additional dialogs ") 
+
+
+// Question 5
+// var name =(" JavaScript Alert \n \n \n")
+// var name2 =("Hello...! I can run JS through my web browsers console ")
+// console.log(name + name2)
+
+// Question 6
+// alert("Hello")
+
+// CHAPTER 2
+// Question 1
+// var username 
+// Question 2
+// var myName = ("Safa Mushtaq")
+// console.log(myName)
+// Question 3
+//  var message = ("Hello World")
+//  alert("Hello World")
+//  Question 4
+// alert("Jhon doe")
+// alert("15 years old")
+// alert("Certifies App develoment")
+// Question 5
+// var age =("PIZZA \n PIZZ \n PIZ \n PI \n P ")
+// alert(age)
+// Question 6
+//  var email =("My email address is example@example.com")
+//  alert("email")
+// Question 7
+// var book =(A smarter way to learn JavaScript.)
+// alert("I am trying to learn from the Book A smarter way to learn Javascript")
+// Question 8
+// document.write("Yah! I can write HTML content through JavaScript")
+// Question 9
+//  var name =("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+//  alert(name)
+
+// chapter 3
+// Question 1
+// var age ="i am 15 years old"
+// alert(age)
+// Question 2
+//  var num ="you have visited this site 14 times"
+// alert(num)
+// Question 3
+//  var birthyear ="My birth year is 2005"
+//   document.write(birthyear)
+// Question 4
+// var brand ="John Doe ordered 5 T-shirt(s) on XYZ Clothing store”
+//   document.write(brand)
+
+// chapter 4
+// Question 1
+// var first_name ="10",middle_name="80",last_name="30"
+// console.log(first_name,middle_name,last_name)
+// Question 2
+// legal variables
+// A variable name contain only letters is legal
+// A variable name contain only numbers is legal
+// A variable name contain only dollar sign is legal
+// A variable name contain only underscore is legal
+// camelCase naming is legal
+
+
+// illegal variables
+
+// enclose in quotation
+// name in a Number
+// start with a Number
+// JavaScript key word
+// giving var space
+// Question 3
+//  document.write( "a) A heading stating “Rules for naming JS variables” <br> b)Variable names can only contain____,____<br>____,_____. <br> c) Variables must begin with a____,_____or<br>_____.For Example $name,___name or name <br> d) Variable names are case__________<br> e) Variable names should not be JS_________")
+
+// Chapter 1 (Alerts)
+
+// 1. Alert these following (individually):
+// I. First Name
+// alert(first Name)
+// II. Last Name
+// alert(Last Name)
+// III. Email
+// alert(Email)
+// IV. Phone Number 
+// alert(Phone Number)
+// V. Password
+// alert(Password)
+// 2. Correct this statement: alert &quot;You&#39;re learning JavaScript!&quot; ;
+//  alert("You'are learning JavaScript!");
+// 3. Code an alert statement displaying any message you like.
+//  var message =("this is safa");
+//  alert(message);
+
+// Chapter 2 (Variables for string)
+// 1. Declare any variable in the camel Case format.
+// answer
+// var happyFood ="pizza";
+
+// 2. Declare a variable of your choice without defining it. Then, in a
+// second statement, assign it a string of your choice.
+// answer
+// var message="";
+// document.write(message)
+
+// 3. Declare the variable teamName and Alert your Team name.
+
+// answer
+// var teamName ="";
+// alert(teamName)
+
+// 4. This statement has already been coded. var bestMan = &quot;Charlie&quot;;
+// Assign the variable a new string.
+
+// answer
+// var bestMan = "jhon";
+
+// Chapter 3 (Variables for numbers)
+// 1. Declare a variable “caseQty”
+
+// answer
+//  var caseQty =10;
+
+// 2. Assign to the variable caseQty, which has already been declared,
+// the value 144.
+
+// answer
+// var num =144;
+
+// 3. Rewrite this statement so the variable can be used in a math
+// operation. var num = &quot;9&quot;;
+
+// answer
+// var num =9;
+
+
+
+
